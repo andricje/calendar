487ef2b46c7edf33ca43f4fb268c72fbf52cc15b
Richard <richardwebdesigner@me.com>
2024-01-08 02:34:00 +0000

    Add tests for UFC events

--- tests/test_calendar_control.py
+++ tests/test_calendar_control.py
@@ -11,16 +11,16 @@ def teardown_module(module):
     print("*****TEARDOWN*****")
 
 
-def test_get_event_links(calendar_control: CalendarControl):
-    event_links = calendar_control.get_event_links()
+def test_get_event_links_onefc(one_fc: CalendarControl):
+    event_links = one_fc.get_event_links()
     for event in event_links:
         assert "https://www.onefc.com" in event
 
 
-def test_get_event_from_url(calendar_control: CalendarControl):
-    event_links = calendar_control.get_event_links()
+def test_get_event_from_url_onefc(one_fc: CalendarControl):
+    event_links = one_fc.get_event_links()
     for link in event_links:
-        event = calendar_control.get_event_from_url(link)
+        event = one_fc.get_event_from_url(link)
         assert event
         assert type(event) == Event
         assert event.begin
@@ -30,8 +30,34 @@ def test_get_event_from_url(calendar_control: CalendarControl):
         assert event.url
 
 
-def test_update_calendar(calendar_control: CalendarControl):
-    file_path = calendar_control.update_calendar()
+def test_update_calendar_onefc(one_fc: CalendarControl):
+    file_path = one_fc.update_calendar()
+    assert file_path
+    assert type(file_path) == PosixPath
+    assert file_path.exists()
+
+
+def test_get_event_links_ufc(ufc: CalendarControl):
+    event_links = ufc.get_event_links()
+    for event in event_links:
+        assert "https://www.ufc.com" in event
+
+
+def test_get_event_from_url_ufc(ufc: CalendarControl):
+    event_links = ufc.get_event_links()
+    for link in event_links:
+        events = ufc.get_events_from_url(link)
+        for event in events:
+            assert event
+            assert type(event) == Event
+            assert event.begin
+            assert event.end
+            assert event.name
+            assert event.url
+
+
+def test_update_calendar_ufc(ufc: CalendarControl):
+    file_path = ufc.update_calendar()
     assert file_path
     assert type(file_path) == PosixPath
     assert file_path.exists()
